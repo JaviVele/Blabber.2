60f5cf9db3f3d19f3f39d7bbd8c0a02abe33f551
alberto-mulero <carreromuleroalberto@gmail.com>
2023-05-27 18:33:59 +0200

    columna izquierda medio hecha

--- frontend/src/app/inicio/inicio.component.ts
+++ frontend/src/app/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BackendService } from '../services/backend.service';
 
@@ -8,27 +8,25 @@ import { BackendService } from '../services/backend.service';
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit {
+export class InicioComponent {
 
   id: number | undefined;
+  usuario: any;
 
-  constructor(private route: ActivatedRoute, private backandservice: BackendService ) { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private backandservice: BackendService ) {
     this.route.params.subscribe(params => {
       this.backandservice.listarUno(params['id']).subscribe(
         response => {
-          console.log(response);
+          this.usuario = response;
         },
         error => {
           console.log(error);
         }
 
       );
-      
-      // Aquí puedes utilizar el valor del parámetro `id` como desees
+
+      // Aquí puedes utilizar el valor del parámetro id como desees
     });
-  }
+   }
 
 }
-
