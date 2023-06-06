2f4c08e8cb8115c705e42884e79bca29525f5468
alberto-mulero <carreromuleroalberto@gmail.com>
2023-06-06 22:06:36 +0200

    arreglado

--- backend/api/models/Usuario.js
+++ backend/api/models/Usuario.js
@@ -34,6 +34,14 @@ module.exports = {
       type: 'string',
       allowNull: true
     },
+    seguidores: {
+      collection: 'Seguidores',
+      via: 'seguido_id'
+    },
+    seguidos: {
+      collection: 'Seguidores',
+      via: 'seguidor_id'
+    }
     // Otros atributos del usuario
   },
   // beforeCreate: function (values, next) {
